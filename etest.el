dac98f6fb87626730872f18dd6f1f54241ec1c58
Rodrigo Campos <rod.apd@gmail.com>
2021-11-13 06:56:02 -0300

    feat(npm): add has-package helper

---
+++ etest.el
@@ -0,0 +1,11 @@
+(defun etest--npm-has-package (package)
+  "Returns PACKAGE version if found, nil otherwise."
+  (let* ((json-hash (with-temp-buffer
+                      (insert-file-contents "package.json")
+                      (json-parse-buffer)))
+         (dependencies (gethash "dependencies" json-hash))
+         (devDependencies (gethash "devDependencies" json-hash)))
+    (or (and dependencies (gethash package dependencies))
+        (and devDependencies (gethash package devDependencies)))))
+
+(provide 'etest)
---
+++ test/etest-test.el
@@ -0,0 +1,13 @@
+(require 'ert)
+(require 'etest)
+
+(defconst test-file-location (file-name-directory (or load-file-name buffer-file-name)))
+
+(ert-deftest etest-npm-has-package ()
+  "Should return PACKAGE version if found, nil otherwise."
+  (let ((default-directory (concat test-file-location "fixtures/npm/mocha/")))
+    (should (equal (etest--npm-has-package "mocha") "1.0.0")))
+  (let ((default-directory (concat test-file-location "fixtures/npm/jest/")))
+    (should (equal (etest--npm-has-package "jest") "1.0.0")))
+  (let ((default-directory (concat test-file-location "fixtures/npm/empty/")))
+    (should (equal (etest--npm-has-package "jest") nil))))
