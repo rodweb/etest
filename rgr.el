6035a53dfa974c67ab12b243984198085eef682f
Rodrigo Campos <rod.apd@gmail.com>
2022-07-30 08:55:57 -0300

    feat: add rgr-last

--- rgr.el
+++ rgr.el
@@ -57,12 +57,20 @@
   "Return the current filename."
   (buffer-file-name))
 
+(defvar rgr-last-command
+  "Last command ran." nil)
+
 (defun rgr--run (&rest args)
   "Run test runner's command-args function with the given ARGS."
-  (let* ((default-directory (funcall rgr-project-root-function))
-         (runner (rgr--guess-project-runner))
-         (command (rgr--call-if-bound runner "command-args" args)))
-    (compile (mapconcat #'identity command " "))))
+  (let ((default-directory (funcall rgr-project-root-function))
+        (compile-command nil))
+    (if (plist-get args :repeat)
+        (compile rgr-last-command)
+      (let* ((runner (rgr--guess-project-runner))
+             (command (rgr--call-if-bound runner "command-args" args))
+             (command-string (mapconcat #'identity command " ")))
+        (setq rgr-last-command command-string)
+        (compile command-string)))))
 
 (defun rgr-project ()
   "Run project tests."
@@ -79,6 +87,11 @@
   (interactive)
   (rgr--run :dwim t))
 
+(defun rgr-last ()
+  "Repeat last test run."
+  (interactive)
+  (rgr--run :repeat t))
+
 (defcustom rgr-project-root-function #'projectile-project-root
   "Function to query for the project root."
   :group 'rgr-mode)
@@ -91,6 +104,7 @@
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "p") #'rgr-project)
     (define-key map (kbd "f") #'rgr-file)
+    (define-key map (kbd "l") #'rgr-last)
     (define-key map (kbd "t") #'rgr-dwim)
     map)
   "Keymap for `rgr-mode' commands.")
